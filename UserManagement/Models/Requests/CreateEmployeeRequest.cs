c8df5e597d738ee59b2bdc18c3afe51a03cc5c4c
mateicoman <mateicoman01@gmail.com>
2023-03-01 12:55:09 +0000

    add employee requests

---
+++ UserManagement/Models/Requests/CreateEmployeeRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.Models.Requests
+{
+	public class CreateEmployeeRequest
+	{
+        [Required]
+        public string? Forename { get; init; }
+        [Required]
+        public string? Surname { get; init; }
+        [Required]
+        public string? Username { get; init; }
+        [Required]
+        public int DepartmentId { get; init; }
+        [Required]
+        public int SiteId { get; init; }
+        [Required]
+        [EmailAddress]
+        public string? Email { get; init; }
+        public string? PhotoFileName { get; set; }
+    }
+}
+
---
+++ UserManagement/Models/Requests/UpdateEmployeeRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.Models.Requests
+{
+	public class UpdateEmployeeRequest
+	{
+        public string? Forename { get; init; }
+        public string? Surname { get; init; }
+        public string? Username { get; init; }
+        public int DepartmentId { get; init; }
+        public int SiteId { get; init; }
+        [EmailAddress]
+        public string? Email { get; init; }
+        public string? PhotoFileName { get; set; }
+    }
+}
+
